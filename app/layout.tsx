296a5087e5cf286d073fe39ccead979169efea38
eddb <eddblower@gmail.com>
2024-11-21 23:20:35 -0600

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Music Matching Game',
+  description: 'Match the songs to their submitters!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
