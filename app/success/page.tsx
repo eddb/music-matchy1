d3533cbd157e35c741a2c59b2389748126fe1dd2
eddb <eddblower@gmail.com>
2024-11-23 14:53:53 +0000

    Update page.tsx

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { supabase } from '.,/supabaseClient';
+import { supabase } from '../supabaseClient';
 
 interface Song {
   title: string;
