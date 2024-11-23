9dbb620dc65145ef9adc40e9b8acfcce95b1f7e1
eddb <eddblower@gmail.com>
2024-11-23 13:22:29 +0000

    Update songsubmissionform.tsx

--- components/songsubmissionform.tsx
+++ components/songsubmissionform.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { supabase } from '@/lib/supabase';
+import { supabase } from '../lib/supabase';
 
 export default function SongSubmissionForm() {
   const [name, setName] = useState('');
