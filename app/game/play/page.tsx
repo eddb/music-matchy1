5ce756a4d7c1fff4f584dff60132db4a186a7298
eddb <eddblower@gmail.com>
2024-11-24 13:36:17 +0000

    Update page.tsx

--- app/game/play/page.tsx
+++ app/game/play/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../../supabase';
 
 if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
   console.error('Missing Supabase environment variables');
