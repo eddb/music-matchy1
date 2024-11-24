9cf433287371c67aaad0c71f605459c798698af0
eddb <eddblower@gmail.com>
2024-11-24 13:10:28 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import './globals.css'
+import { Cherry_Bomb_One } from 'next/font/google'
+
+const cherryBomb = Cherry_Bomb_One({ 
+  weight: '400',
+  subsets: ['latin']
+})
 
 export const metadata = {
-  title: 'Music Matching Game',
-  description: 'Match the songs to their submitters!',
+  title: 'GUESSIC',
+  description: 'The music matching game',
 }
 
 export default function RootLayout({
@@ -12,7 +18,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <link href="https://fonts.cdnfonts.com/css/share-tech-mono" rel="stylesheet" />
+        <link rel="stylesheet" href="https://departuremono.com/departure-mono.css" />
+      </head>
+      <body className={cherryBomb.className}>
+        {children}
+        <div className="fixed bottom-4 right-4 text-sm text-gray-500 font-mono">
+          made by @edd_b
+        </div>
+      </body>
     </html>
   )
 }
