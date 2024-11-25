a164c1b2a68cece68172dc1affbbf3c281df0aa4
eddb <eddblower@gmail.com>
2024-11-25 16:24:38 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Cherry_Bomb_One } from 'next/font/google'
-import Image from 'next/image'
 
 const cherryBomb = Cherry_Bomb_One({ 
   weight: '400',
@@ -23,20 +22,20 @@ export default function RootLayout({
         <link href="https://fonts.cdnfonts.com/css/share-tech-mono" rel="stylesheet" />
         <link rel="stylesheet" href="https://departuremono.com/departure-mono.css" />
       </head>
-      <body className={cherryBomb.className}>
+      <body className={`${cherryBomb.className} min-h-screen`} style={{ backgroundColor: '#d4ccca' }}>
         <div 
-  className="fixed inset-0 z-0"
-  style={{
-    backgroundImage: 'url(/back.jpg)',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat'
-  }}
-/>
-        <div className="relative z-10">
+          className="fixed inset-0 w-full h-[380px] z-0"
+          style={{
+            backgroundImage: 'url("/back.png")',
+            backgroundPosition: 'center top',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain',
+          }}
+        />
+        <div className="relative pt-[360px] min-h-screen z-10">
           {children}
         </div>
-        <div className="fixed bottom-4 right-4 text-sm text-gray-500 font-tech">
+        <div className="fixed bottom-4 right-4 text-sm text-gray-500 font-tech z-50">
           made by @edd_b
         </div>
       </body>
